67930db902d35a967bdeccd126cfc23e12d070ba
Ayush-Jaiswal25 <ayushjaiswal5309@gmail.com>
2024-05-15 14:27:54 +0530

    updated the images

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Navbar.css'
 import { Link } from 'react-router-dom'
+import User from '../../assets/nav_user.png'
+import HamMenu from '../../assets/nav_hamMenu.png'
 function Navbar() {
   return (
     <div className="nav">
@@ -13,10 +15,10 @@ function Navbar() {
             <label class="jobs-explo" id="EXPLORE" for="exploreCheck" onclick="clickExplore()">EXPLORE</label>
             <input id="exploreCheck" type="checkbox" checked />
             <a class="jobs-explo" href="/prowork/jobs">ABOUT US</a>
-            <a id="user-btn" onclick="clickUser()"><img src="/src/assets/nav_user.png"/></a>
+            <a id="user-btn" onclick="clickUser()"><img src={User} /></a>
           </div>
           <div class="navbar-options-phone">
-          <a id="user-btn" onclick="clickUser()"><img src="/src/assets/nav_hamMenu.png"/></a>
+          <a id="user-btn" onclick="clickUser()"><img src={HamMenu} /></a>
           </div>
         </div>
       </div>
